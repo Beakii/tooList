e365062bd498c3fcd54279cb49fe9f33df8eb445
Beakii <auminist3@gmail.com>
2020-04-20 17:12:58 +1200

    Updates

---
+++ precache-manifest.fa03bd9200d73d8c2829f4a462b0f323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc465c5099d87c329d1a0703c16a813",
+    "url": "/lisTool/index.html"
+  },
+  {
+    "revision": "422b383befb578e58083",
+    "url": "/lisTool/static/css/main.6c17713f.chunk.css"
+  },
+  {
+    "revision": "5438b9f3ee6b2e1e2c60",
+    "url": "/lisTool/static/js/2.5a37d1bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lisTool/static/js/2.5a37d1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422b383befb578e58083",
+    "url": "/lisTool/static/js/main.5419132f.chunk.js"
+  },
+  {
+    "revision": "98677398105d2b36dca0",
+    "url": "/lisTool/static/js/runtime-main.938cb2e1.js"
+  }
+]);
